8829f49c61d08e99f754f20090821d17be698374
Elan Deyan <deyancaio83@hotmail.com>
2024-02-05 20:51:46 -0300

    test: implements some tests for the class
    Until this moment, all tests passed

--- stack/python/test.py
+++ stack/python/test.py
@@ -1,1 +1,94 @@
+from stack import Stack
+
 import unittest
+
+from state_error import EmptyStackError
+
+
+class TestStack(unittest.TestCase):
+    def test_isinstance(self):
+        maybe_stack = Stack[str]()
+
+        maybe_stack_from_sequence = Stack[int].from_sequence([1, 2, 3, 4, 5])
+
+        self.assertIsInstance(maybe_stack, Stack)
+
+        self.assertIsInstance(maybe_stack_from_sequence, Stack)
+
+    def test_len(self):
+        empty_stack = Stack[int]()
+
+        self.assertEqual(len(empty_stack), 0)
+
+        stack_from_sequence = Stack[int].from_sequence([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+        self.assertEqual(len(stack_from_sequence), 10)
+
+    def test_contains(self):
+        stack = Stack[float]()
+
+        stack.push(1.0)
+
+        self.assertIn(1.0, stack)
+
+        another = Stack[str].from_sequence(["oi", "tudo", "bem", "?"])
+
+        self.assertIn("oi", another)
+
+    def test_emptiness(self):
+        empty = Stack[int]()
+
+        self.assertEqual(len(empty), 0)
+
+        self.assertTrue(empty.is_empty)
+
+        another_stack = Stack[str]()
+
+        another_stack.push("oieeeee")
+        another_stack.push("how're you?")
+
+        self.assertFalse(another_stack.is_empty)
+
+        from_sequence = Stack[int].from_sequence([1, 2, 3, 4, 5])
+
+        self.assertFalse(from_sequence.is_empty)
+
+    def test_push(self):
+        stack = Stack[int]()
+
+        stack.push(5)
+
+        self.assertIn(5, stack)
+
+        stack.push(3)
+        self.assertIn(3, stack)
+
+    def test_pop(self):
+        stack = Stack[int].from_sequence([1,2,3,4,5,6,7,8,9,10])
+
+        poped_item = stack.pop()
+
+        self.assertEqual(poped_item, 10)
+
+        another_poped = stack.pop()
+
+        self.assertEqual(another_poped, 9)
+
+        empty = Stack[int]()
+
+        self.assertRaises(EmptyStackError, empty.pop)
+
+    def test_try_pop(self):
+        stack = Stack[int].from_sequence([1])
+
+        poped_item = stack.try_pop()
+
+        self.assertEqual(poped_item, 1)
+
+        maybe_int = stack.try_pop()
+
+        self.assertIsNone(maybe_int)
+
+
+if __name__ == "__main__":
+    unittest.main()
