04ba89056a3884a92e5f2f8e74bfcfa0e8ad5805
Elan Deyan <deyancaio83@hotmail.com>
2024-02-05 20:06:00 -0300

    fix: fixes type hint syntax

--- stack/python/example.py
+++ stack/python/example.py
@@ -3,8 +3,8 @@ from stack import Stack
 
 if __name__ == "__main__":
     # constructors
-    empty_stack: Stack[int] = Stack()
-    stack_from_list: Stack[int] = Stack.from_sequence([1, 2, 3, 4, 5, 6, 7])
+    empty_stack = Stack[int]()
+    stack_from_list = Stack[int].from_sequence([1, 2, 3, 4, 5, 6, 7])
 
     print(empty_stack.is_empty)  # True
     print(stack_from_list.is_empty)  # False
