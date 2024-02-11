e0a9dbae3c7d89eb13f05203244c2bd84e8dc622
Elan Deyan <deyancaio83@hotmail.com>
2024-02-10 22:22:20 -0300

    feat: adds clear and __iter__ methods Also improves generics following mypy suggestions

--- stack/python/stack.py
+++ stack/python/stack.py
@@ -1,11 +1,14 @@
-from typing import Collection, Iterator, Self, override
+from typing import Collection, Generic, Iterator, Self, TypeVar, override
 from collections.abc import Sequence
 
 from state_error import EmptyStackError
 
+E = TypeVar("E")
 
-class Stack[E](Collection):
+
+class Stack(Collection, Generic[E]):
     _internal_list: list[E]
+    _iterator_index: int = -1
 
     def __init__(self) -> None:
         super().__init__()
@@ -13,11 +16,11 @@ class Stack[E](Collection):
 
     @classmethod
     def from_sequence(cls, sequence: Sequence[E]) -> Self:
-        stack: Stack[E] = cls()
+        stack = cls()
 
         for item in sequence:
             stack.push(item)
-        
+
         return stack
 
     def push(self, element: E) -> None:
@@ -34,14 +37,17 @@ class Stack[E](Collection):
             return self.pop()
         except EmptyStackError:
             return None
-        
+
+    def clear(self) -> None:
+        self._internal_list.clear()
+
     @property
     def peek(self) -> E:
         if len(self._internal_list) > 0:
             return self._internal_list[-1]
         else:
             raise EmptyStackError()
-        
+
     @property
     def try_peek(self) -> E | None:
         try:
@@ -52,15 +58,25 @@ class Stack[E](Collection):
     @property
     def is_empty(self) -> bool:
         return len(self._internal_list) == 0
-    
+
     @override
     def __len__(self) -> int:
         return len(self._internal_list)
-    
+
     @override
-    def __contains__(self, element: E) -> bool:
+    def __contains__(self, element: object) -> bool:
         return element in self._internal_list
-    
+
     @override
     def __iter__(self) -> Iterator:
-        raise NotImplementedError()
+        return self
+
+    def __next__(self) -> E:
+        if self._iterator_index >= -len(self._internal_list):
+            item = self._internal_list[self._iterator_index]
+            self._iterator_index -= 1
+
+            return item
+        else:
+            self._iterator_index = -1
+            raise StopIteration
